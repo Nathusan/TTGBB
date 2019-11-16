8045db783253dfb94762492b4b9563b4d36858bf
gnash-crazy-rat <57827358+gnash-crazy-rat@users.noreply.github.com>
2019-11-16 18:31:51 +0000

    Transform Character
    -Ball added

---
+++ TTGBB/Zachs/Zachs/3TG/Assets/MushroomHit.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MushroomHit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEvent(Collider other)
+    {
+        if(other.gameObject.Compare("Player"))
+        {
+            if(gameObject.tag == Round)
+            {
+                other.
+            }
+        }
+    }
+}
