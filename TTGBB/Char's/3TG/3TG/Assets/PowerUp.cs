a365ec84dc00382ba9e2875b8e59264a96e2e062
Charlotte <choclit9@gmail.com>
2019-11-16 11:26:03 +0000

    Chars

---
+++ TTGBB/Char's/3TG/3TG/Assets/PowerUp.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour
+{
+
+    public GameObject pickupEffect;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PickUp(other);
+        }      
+    }
+
+    void PickUp(Collider player)
+    {
+        //Spawn cool effect
+        Instantiate(pickupEffect, transform.position, transform.rotation);
+
+        //Apply effect to the player
+
+
+        //Remove power up object
+        Destroy(gameObject);
+    }
+}
